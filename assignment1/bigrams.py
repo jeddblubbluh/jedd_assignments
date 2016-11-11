6fc9b9e40b5f577c704f657b29d3cde240f120d4
salim <slpg500a>
2016-11-11 21:34:53 +0000

    Add loads of stuff

--- assignment1/bigrams.py
+++ assignment1/bigrams.py
@@ -2,17 +2,25 @@ from typing import List, Mapping
 import io
 
 
-def calculate_bigrams(number:int, stream:io.TextIOWrapper)->List[str]:
-    return ["xx", "xx", "xx"]
+def calculate_bigrams(number: int, stream: io.TextIOWrapper)->List[str]:
+    """
+    :param number: The number of bigrams you want to return
+    :param stream: A text stream, the result of opening a file
+    :return: List of bigrams
+    """
+    return ["xx"] * number
 
 
 def print_result(result:Mapping[str,List[str]])->None:
+    """
+    :param result: Mapping of language name to the most common bigrams
+    :return:
+    """
     print("Hello!")
 
-
-def main(number:int=5):
+def main(number: int=5):
     result = {}
-    for language in ["eglish", "welsh"]:
+    for language in ["english", "welsh"]:
         filename = "%s.txt" % language
         result[language] = calculate_bigrams(number, open(filename))
 
